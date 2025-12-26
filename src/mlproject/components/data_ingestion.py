a494ec0cdba63164cf44833c6017a7ad9202528f
PROXLR8 <prodiptoh63@gmail.com>
2025-12-26 02:06:30 +0100

    model trainer

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -12,6 +12,9 @@ from dataclasses import dataclass
 from src.mlproject.components.data_transformation import DataTransformation
 from src.mlproject.components.data_transformation import DataTransformationConfig
 
+from src.mlproject.components.model_trainer import ModelTrainerConfig
+from src.mlproject.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join("artifacts", "train.csv")
@@ -75,4 +78,10 @@ if __name__ == "__main__":
     train_data, test_data=obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(
+        train_array=train_arr,
+        test_array=test_arr
+    ))
